9b30906d7120feeb064c2bde7bfc0c1c102fea88
guicbelon <guilherme.c.belon@gmail.com>
2022-10-23 18:08:42 -0300

    teste4

---
+++ src/components/Inpts/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles';
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type InptProps = {};
+
+// eslint-disable-next-line no-empty-pattern
+const Inpt: React.FC<InptProps> = ({}) => <S.Cont> oi</S.Cont>;
+
+export default Inpt;
