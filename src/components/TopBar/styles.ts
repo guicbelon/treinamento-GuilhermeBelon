f250af3a55b3c030e21817fd1123747ba66f3103
guicbelon <guilherme.c.belon@gmail.com>
2022-10-23 22:03:46 -0300

    c8

---
+++ src/components/TopBar/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Cont = styled.div`
+    border-radius: 16px;
+    height: 64px;
+    background: rgba(93, 89, 102, 0.5);
+    align-items: center;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+`;
+
+export const Prof = styled.img`
+    height: 32px;
+    cursor: pointer;
+`;
+
+export const Search = styled.img`
+    height: 32px;
+    cursor: pointer;
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from 'next';
-import HomeTemplate from 'template/Home';
+import HomeTemplate from 'template/Feed';
 
 const Home: NextPage = () => {
     return <HomeTemplate />;
